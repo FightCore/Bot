000ada1a4e59d8ae1848e585e43c8a185171b3aa
Bart de Bever <bartdebever112@gmail.com>
2020-09-07 17:28:20 +0200

    Up embed version

--- FightCore.Bot/FightCore.Bot/EmbedCreators/Base/BaseEmbedCreator.cs
+++ FightCore.Bot/FightCore.Bot/EmbedCreators/Base/BaseEmbedCreator.cs
@@ -50,7 +50,7 @@ namespace FightCore.Bot.EmbedCreators.Base
 
         protected EmbedBuilder AddFooter(EmbedBuilder builder)
         {
-            const string version = "v1.0.3 DEV";
+            const string version = "v1.0.4";
             switch (EmbedSettings.FooterType)
             {
                 case FooterType.FightCore:
