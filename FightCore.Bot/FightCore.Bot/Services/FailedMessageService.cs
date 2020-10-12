6e0fadda58387dc91c39d8e79bb40dcf0134a6a6
Bart de Bever <bartdebever112@gmail.com>
2020-10-12 17:20:52 +0200

    Add a timer to remove old failed messages.

--- FightCore.Bot/FightCore.Bot/Services/FailedMessageService.cs
+++ FightCore.Bot/FightCore.Bot/Services/FailedMessageService.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Net.NetworkInformation;
+using System.Timers;
 using Discord;
 using Discord.WebSocket;
+using Microsoft.Extensions.Logging;
 
 namespace FightCore.Bot.Services
 {
@@ -9,9 +11,23 @@ namespace FightCore.Bot.Services
     {
         private readonly Dictionary<ulong, IUserMessage> _failedMessages;
 
-        public FailedMessageService()
+        private const double TimerInterval = 1000 * 60 * 2;
+        private readonly LogService _logger;
+
+        public FailedMessageService(LogService logger)
         {
+            _logger = logger;
             _failedMessages = new Dictionary<ulong, IUserMessage>();
+        }
+
+        private void DeleteFailedMessage(ulong failedId)
+        {
+            if (!_failedMessages.ContainsKey(failedId))
+            {
+                return;
+            }
+
+            _failedMessages.Remove(failedId);
         }
 
         public void AddFailedMessaged(ulong failedId, IUserMessage responseId)
@@ -22,6 +38,11 @@ namespace FightCore.Bot.Services
             }
 
             _failedMessages.Add(failedId, responseId);
+
+            // Start a timer for 2 minutes, after this time is over, remove the message with the failed id.
+            var timer = new Timer(TimerInterval);
+            timer.Elapsed += (sender, args) => DeleteFailedMessage(failedId);
+            timer.Start();
         }
 
         public IUserMessage GetMessageIdForEditMessage(ulong messageId)
